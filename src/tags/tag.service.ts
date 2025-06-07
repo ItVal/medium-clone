56339ac8e37ec3a0f4d37b8b6656a06cdf01ae3b
Valentin Nasibu <irnasvalentin@gmail.com>
2025-06-07 12:32:43 +0200

    feat:findAll in db

--- src/tags/tag.service.ts
+++ src/tags/tag.service.ts
@@ -1,8 +1,16 @@
 import { Injectable } from "@nestjs/common";
+import { promises } from "dns";
+import { TagEntity } from "./tag.entity";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Repository } from "typeorm";
 
 @Injectable()
 export class TagService {
- findAll(){
-    return ["VB, Akim, Gloire"];
+    constructor(
+        @InjectRepository(TagEntity)
+        private readonly tagRepository: Repository<TagEntity>,  
+    ) {}
+ async findAll(): Promise<TagEntity[]> {
+  return await this.tagRepository.find();
  }
 }
